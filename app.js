7314cf7fd16f91755b4b0c49186102f6a939e9fb
Igor051 <snigurigor05116@gmail.com>
2022-05-02 10:04:42 +0300

    connected to the database and installed the necessary dependencies

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
+require("./src/configs/database").connect();
 const express = require("express");
 
 const app = express();
+
+app.use(express.json());
 
 module.exports = app;

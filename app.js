7a5f76c8097e19f66512b6a91d5fa0bfd4238ea2
Igor051 <snigurigor05116@gmail.com>
2022-05-05 21:47:20 +0300

    create 404 not Found

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ require("./src/configs/database").connect();
 const express = require("express");
 const app = express();
 
-const registerRouter = require("./src/routes/registerRouter");
-const loginRouter = require("./src/routes/loginRouter");
+const {registerRouter, loginRouter} = require("./src/routes");
+
+const {notFoundErrorController} = require("./src/controllers/notFoundErrorController");
 
 app.use(express.json());
 
 app.use("/register", registerRouter);
 app.use("/login", loginRouter);
+
+app.all("*", notFoundErrorController)
 
 module.exports = app;

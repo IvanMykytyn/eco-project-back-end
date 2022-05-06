854afd2e5aecbe36ed00478d33caf9ee248811ca
Igor051 <snigurigor05116@gmail.com>
2022-05-06 12:08:09 +0300

    create the CORS for any origin

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 require("./src/configs/database").connect();
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
@@ -8,6 +9,9 @@ const {registerRouter, loginRouter} = require("./src/routes");
 const {notFoundErrorController} = require("./src/controllers/notFoundErrorController");
 
 app.use(express.json());
+app.use(cors({
+    origin: "*"
+}));
 
 app.use("/register", registerRouter);
 app.use("/login", loginRouter);

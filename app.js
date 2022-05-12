09fa0cc8ef57bc8ab1ff979c0a89f2df69b8b874
VanekCheck <mykytynivan04@gmail.com>
2022-05-12 20:52:09 +0300

    Update OAuth2: connecting with front-end

--- app.js
+++ app.js
@@ -5,28 +5,60 @@ const express = require("express");
 const app = express();
 
 const {
-  GoogleAuthRedirect,
-  GoogleAuthController,
+  getGoogleAuthURL,
+  getGoogleUser,
 } = require("./src/controllers/GoogleAuthController");
 
 const { registerRouter, loginRouter } = require("./src/routes");
 
 const {
+  createUserByGoogle,
+  findUserByEmail,
+} = require("./src/services/userService");
+
+const jwt = require("jsonwebtoken");
+
+//
+const {
   notFoundErrorController,
 } = require("./src/controllers/notFoundErrorController");
 
 app.use(express.json());
-app.use(cors({
-    origin: "*"
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/register", registerRouter);
 app.use("/login", loginRouter);
 
-// url for register or login
-app.get("/auth/google", GoogleAuthRedirect);
-// url for redirecting
-app.get("/auth/google/profile", GoogleAuthController);
+app.get("/auth/google", (req, res) => {
+  try {
+    return res.status(200).json({ redirectUrl: getGoogleAuthURL() });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/profile", async (req, res) => {
+  const data = await getGoogleUser(req.query);
+  const { id, email, given_name, family_name } = data;
+
+  var user = await findUserByEmail(email);
+  if (!user) {
+    user = await createUserByGoogle(given_name, family_name, email);
+  }
+  // Create token
+  const token = jwt.sign({ userId: user._id }, process.env.TOKEN_KEY, {
+    expiresIn: "1d",
+  });
+
+  // save user token
+  user.token = token;
+  // https://eco-ntinuum.herokuapp.com/profile
+  res.redirect("http://localhost:3001/profile", 200, user);
+});
 
 app.all("*", notFoundErrorController);
 
--- src/controllers/GoogleAuthController.js
+++ src/controllers/GoogleAuthController.js
@@ -1,18 +1,12 @@
-const {
-  createUserByGoogle,
-  findUserByEmail,
-} = require("../services/userService");
-
 const axios = require("axios");
 const { google } = require("googleapis");
-const jwt = require("jsonwebtoken");
 
 // CLIENT_ID - CLIENT_SECRET - REDIRECT_URL
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  // https://eco-project-back-end.herokuapp.com/auth/google/profile
-  "http://localhost:3000/auth/google/profile"
+  // https://eco-project-back-end.herokuapp.com/profile
+  "http://localhost:3000/profile"
 );
 
 function getGoogleAuthURL() {
@@ -50,28 +44,4 @@ async function getGoogleUser({ code }) {
   return googleUser;
 }
 
-module.exports = {
-  async GoogleAuthRedirect(req, res) {
-    res.redirect(getGoogleAuthURL());
-  },
-
-  async GoogleAuthController(req, res) {
-    const data = await getGoogleUser(req.query);
-    const { id, email, given_name, family_name } = data;
-
-    var user = await findUserByEmail(email);
-    if (!user) {
-      user = await createUserByGoogle(given_name, family_name, email);
-    }
-
-    // Create token
-    const token = jwt.sign({ userId: user._id }, process.env.TOKEN_KEY, {
-      expiresIn: "1d",
-    });
-
-    // save user token
-    user.token = token;
-
-    res.status(200).json(user);
-  },
-};
+module.exports = { getGoogleAuthURL, getGoogleUser };

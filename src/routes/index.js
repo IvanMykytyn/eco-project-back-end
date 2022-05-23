7e9ea385ac402bd45441330fc17df4466862a2b5
VanekCheck <mykytynivan04@gmail.com>
2022-05-23 22:10:18 +0300

    fixed small bug

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,30 +1,19 @@
 const loginRouter = require("./loginRouter");
 const registerRouter = require("./registerRouter");
+const activitiesRouter = require("./activitiesRouter");
+const taskRouter = require("./taskRouter");
+const taskHistoryRouter = require("./taskHistoryRouter");
+const userInformationRouter = require("./userInformationRouter");
+const ratingRouter = require("./ratingRouter");
 const googleAuthRouter = require("./googleAuthRouter");
 
 module.exports = {
   loginRouter,
   registerRouter,
+  activitiesRouter,
+  taskRouter,
+  taskHistoryRouter,
+  userInformationRouter,
+  ratingRouter,
   googleAuthRouter,
 };
-
-const loginRouter = require("./loginRouter")
-const registerRouter = require("./registerRouter")
-const activitiesRouter = require("./activitiesRouter")
-const taskRouter = require("./taskRouter")
-const taskHistoryRouter = require("./taskHistoryRouter");
-const userInformationRouter = require("./userInformationRouter")
-const ratingRouter = require("./ratingRouter");
-const googleAuthRouter = require("./googleAuthRouter");
-
-module.exports ={
-        loginRouter,
-        registerRouter,
-        activitiesRouter,
-        taskRouter,
-        taskHistoryRouter,
-        userInformationRouter,
-        ratingRouter,
-        googleAuthRouter
-    }
-

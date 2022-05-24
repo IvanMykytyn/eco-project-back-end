40cfff8f5ef13932fdd0b5d61513dc9d4dc6f1fb
Igor051 <snigurigor05116@gmail.com>
2022-05-24 21:26:36 +0300

    changed user task description required to optional

--- src/models/task.js
+++ src/models/task.js
@@ -6,8 +6,8 @@ const taskSchema = new mongoose.Schema({
     user_id: {type: mongoose.Types.ObjectId, required: true},
     activity_type: {type: String, enum: activityTypeEnum, required: true},
     status: {type: String, enum: statusEnum, required: true},
-    users_task_description: {type: String, required: true},
-    numerical_indicators: {type: Number},
+    users_task_description: {type: String},
+    numerical_indicators: {type: Number, required: true},
     admin_message: {type: String},
     location: {type: String},
     photos: {type: [Buffer], required: true}
